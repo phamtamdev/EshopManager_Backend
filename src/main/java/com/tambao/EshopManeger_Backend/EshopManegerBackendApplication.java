9d8f9cb7981baaf10ce0d9b2cd364ceb3857a8b7
phamvantam <phamvantam1911@gmail.com>
2024-07-09 22:37:53 +0700

    init project

---
+++ src/main/java/com/tambao/EshopManeger_Backend/EshopManegerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tambao.EshopManeger_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopManegerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EshopManegerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tambao/EshopManeger_Backend/EshopManegerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tambao.EshopManeger_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopManegerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
